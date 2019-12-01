75ff26116b0ce35e34444b1f3efea06068925cda
goo2821 <milk5352@gmail.com>
2019-12-01 18:55:15 +0900

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var sequelize = require('./models');
 var viewRouter = require('./routes/view');
 var apiRouter = require('./routes/api');
@@ -13,7 +11,7 @@ var app = express();
 sequelize.sequelize.sync();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -24,21 +22,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', viewRouter);
 app.use('/api', apiRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/view/index.js
+++ routes/view/index.js
@@ -1,21 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index')
 });
-router.get('/algorithm',(req,res) =>{
+
+router.get('/algorithm', (req,res) =>{
   res.render('algorithm')
 });
-router.get('/algorithm/:name',(req,res) =>{
+
+router.get('/algorithm/:name', (req,res) =>{
   res.render('show_contents',{
     name: req.params.name
   })
 });
-router.get('/project',(req,res) =>{
+
+router.get('/project', (req,res) =>{
   res.render('project')
 });
-router.get('/baekjoon',(req,res) =>{
+
+router.get('/baekjoon', (req,res) =>{
   res.render('baekjoon')
 });
 
